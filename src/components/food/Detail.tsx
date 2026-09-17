4b85dacfd7bc9c41b5110fa5b1885ab5d9d01291
chaijewon <vcandjava@nate.com>
2026-09-17 15:45:44 +0900

    1111

--- src/components/food/Detail.tsx
+++ src/components/food/Detail.tsx
@@ -1,8 +1,120 @@
-import {useState,useEffect,Fragment} from "react";
-
+import {useState, useEffect, Fragment, use} from "react";
+import {useNavigate,useParams} from "react-router-dom";
+import apiClient from "../commons/http-commons"
+import KakaoMap from "../commons/KakaoMap";
+/*
+    NO                                        NOT NULL NUMBER
+    NAME                                               VARCHAR2(100)
+    TYPE                                               VARCHAR2(100)
+    PHONE                                              VARCHAR2(30)
+    ADDRESS                                            VARCHAR2(260)
+    SCORE                                              NUMBER(2,1)
+    PARKING                                            VARCHAR2(200)
+    POSTER                                             VARCHAR2(260)
+    TIME                                               VARCHAR2(50)
+    CONTENT                                            CLOB
+    THEME                                              VARCHAR2(4000)
+    PRICE                                              VARCHAR2(100)
+    LIKECOUNT                                          NUMBER
+    JJIMCOUNT                                          NUMBER
+    HIT                                                NUMBER
+    REPLYCOUNT
+ */
+interface FoodDetailData{
+   no:number;
+   name:string;
+   type:number;
+   address:string;
+   phone:string;
+   theme:string;
+   price:string;
+   time:string;
+   parking:string;
+   poster:string;
+   content:string;
+   hit:number;
+   jjimcount:number;
+   replycount:number;
+   likecount:number;
+   score:number;
+}
 function Detail() {
+    const {no} =useParams<{no:string}>() // request.getParameter("no")
+    const nav=useNavigate()
+    const [detail,setDetail]=useState<FoodDetailData|null>(null)
+    //const btnClick=nav(-1)
+    useEffect(()=>{
+        const fetchDetail=async()=>{
+            const res=
+                     await apiClient.get(`/food/detail_react/${no}`)
+            console.log(res)
+            setDetail(res.data)
+            return res.data
+        }
+        fetchDetail()
+    },[])
     return (
-        <h1 className={"text-center"}>Detail</h1>
+        <div className={"container"}>
+            <div className={"row"}>
+                <table className="table">
+                    <tbody>
+                     <tr>
+                         <td width={"30%"} rowSpan={8} className={"text-center"}>
+                             <img src={detail?.poster} style={{"width":'320px',"height":"320px"}} />
+                         </td>
+                         <td colSpan={2}>
+                            <h3>{detail?.name}&nbsp;<span style={{"color":"orange"}}>{detail?.score}</span></h3>
+                         </td>
+                     </tr>
+                    <tr>
+                        <td className={"text-center"} width={"10%"}>주소</td>
+                        <td width={"60%"}>{detail?.address}</td>
+                    </tr>
+                     <tr>
+                         <td className={"text-center"} width={"10%"}>전화</td>
+                         <td width={"60%"}>{detail?.phone}</td>
+                     </tr>
+                     <tr>
+                         <td className={"text-center"} width={"10%"}>음식종류</td>
+                         <td width={"60%"}>{detail?.type}</td>
+                     </tr>
+                     <tr>
+                         <td className={"text-center"} width={"10%"}>주차</td>
+                         <td width={"60%"}>{detail?.parking}</td>
+                     </tr>
+                     <tr>
+                         <td className={"text-center"} width={"10%"}>영업시간</td>
+                         <td width={"60%"}>{detail?.time}</td>
+                     </tr>
+                     <tr>
+                         <td className={"text-center"} width={"10%"}>가격대</td>
+                         <td width={"60%"}>{detail?.price}</td>
+                     </tr>
+                     <tr>
+                         <td className={"text-center"} width={"10%"}>테마</td>
+                         <td width={"60%"}>{detail?.theme}</td>
+                     </tr>
+                    <tr>
+                        <td colSpan={3}>{detail?.content}</td>
+                    </tr>
+                    <tr>
+                        <td colSpan={3} className={"text-right"}>
+                            <button className={"btn btn-danger"}
+                                    onClick={()=>nav(-1)}>목록</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colSpan={3}>
+                            {
+                                detail &&
+                                <KakaoMap address={detail?.address} name={detail?.name}/>
+                            }
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     )
 }
 export default Detail;
--- src/components/main/Home.tsx
+++ src/components/main/Home.tsx
@@ -14,6 +14,24 @@ interface FoodProps{
     startPage:number;
     endPage:number;
 }
+/*
+     Home() <Home/>  http://localhost:3000
+       |
+     mount() => useEffect()
+       |
+     서버로부터 데이터 읽기
+       |
+      저장 => state (state에 저장된 데이터만 HTML에 적용)
+       |
+      return => HTML을 변경해서 index.html
+                              <div id="root">|변경된 HTML을 첨부</div>
+
+      이벤트 발생 (마우스 , 버튼)
+        |
+      setXxx가 호출 되면 => useEffect()를 수행  => 재렌더링
+            ==> setState()
+
+ */
 function Home() {
     const [curpage, setCurpage] = useState<number>(1);
     const [foodData, setFoodData] = useState<FoodProps>();
@@ -30,19 +48,49 @@ function Home() {
     const html=foodData?.list.map((food:Food)=>
         <div className="col-md-3">
             <div className="thumbnail">
-                <a href="#">
+                <Link to={'/food/detail/'+food.no}>
                     <img src={food.poster} alt={food.address} style={{"width":"250px","height":"150px","objectFit":"cover"}} />
                         <div className="caption">
                             <p>{food.name}</p>
                         </div>
-                </a>
+                </Link>
             </div>
         </div>
     )
+    // 이벤트
+    const prev=()=>foodData && setCurpage(foodData.startPage-1);
+    const next=()=>foodData && setCurpage(foodData.endPage+1);
+    const pageChange=(page:number)=>foodData && setCurpage(page);
+    // 페이지
+    const pageArr=[]  // <<
+    if(foodData && foodData.startPage>1){
+        pageArr.push(
+            <li><a className={"nav-link"} onClick={prev}>&laquo;</a></li>
+        )
+    }
+    if(foodData){
+        for(let i:number=foodData.startPage;i<=foodData.endPage;i++){
+            pageArr.push(
+                <li className={i===curpage?"active":""}><a className={"nav-link"} onClick={()=>pageChange(i)}>{i}</a></li>
+            )
+        }
+    }
+    // >>
+    if(foodData && foodData.endPage<foodData.totalpage){
+        pageArr.push(
+            <li><a className={"nav-link"} onClick={next}>&raquo;</a></li>
+        )
+    }
     return (
         <div className="container">
             <div className="row">
                 {html}
+            </div>
+            <div className="row text-center" style={{"marginTop":"10px"}}>
+                <ul className="pagination">
+                    {pageArr}
+                </ul>
+
             </div>
         </div>
     )
